d41b5a794a7d3202e6356bf647840ad6f02c3428
Biswarup Chatterjee <chatterjeebiswarup61@gmail.com>
2024-05-08 17:37:49 +0530

    mui added in login and register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,43 +1,156 @@
-import { useState } from "react"
-export default function Register() {
+import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+// import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+function SignUp() {
     const [form, setForm] = useState({
         firstName: '',
         lastName: '',
         email: '',
         password: '',
         role: ''
-    })
+    });
 
     const handleChange = (e) => {
-        const { name, value } = e.target
-        setForm({ ...form, [name]: value })
+        const { name, value } = e.target;
+        setForm({ ...form, [name]: value });
+    };
 
-    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(form); // This will log the form data when submitted
+        // You can add further logic here, like sending the data to an API
+    };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(form)
-    }
     return (
-        <div>
-            <h2>Register With Us</h2>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor='firstName'>Enter First Name</label> <br />
-                <input type="text" id='firstName' name="firstName" value={form.firstName} onChange={handleChange} /> <br />
-                <label htmlFor='lastName'>Enter Last Name</label> <br />
-                <input type="text" id='lastName' name="lastName" value={form.lastName} onChange={handleChange} /> <br />
-                <label htmlFor='email'>Enter Email</label> <br />
-                <input type="text" id='email' name="email" value={form.email} onChange={handleChange} /> <br />
-                <label htmlFor='password'>Enter Password</label> <br />
-                <input type="password" id='password' name="password" value={form.password} onChange={handleChange} /> <br />
-                <label htmlFor='role'>Select Role</label> <br />
-                <select id="role" name="role" value={form.role} onChange={handleChange}>
-                    <option value="">Select Role</option>
-                    <option value="Employee">Employee</option>
-                    <option value="TeamLead">Team Lead</option>
-                </select> <br /> <br />
-                <input type="submit" />
-            </form>
-        </div>
-    )
+        <ThemeProvider theme={createTheme()}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    autoComplete="given-name"
+                                    name="firstName"
+                                    required
+                                    fullWidth
+                                    id="firstName"
+                                    label="First Name"
+                                    autoFocus
+                                    value={form.firstName}
+                                    onChange={handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="lastName"
+                                    label="Last Name"
+                                    name="lastName"
+                                    autoComplete="family-name"
+                                    value={form.lastName}
+                                    onChange={handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    id="email"
+                                    label="Email Address"
+                                    name="email"
+                                    autoComplete="email"
+                                    value={form.email}
+                                    onChange={handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <TextField
+                                    required
+                                    fullWidth
+                                    name="password"
+                                    label="Password"
+                                    type="password"
+                                    id="password"
+                                    autoComplete="new-password"
+                                    value={form.password}
+                                    onChange={handleChange}
+                                />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <FormControl fullWidth>
+                                    <InputLabel id="demo-simple-select-helper-label">Role</InputLabel>
+                                    <Select
+                                        required
+                                        label="Role"
+                                        name="role"
+                                        labelId="demo-simple-select-helper-label"
+                                        id="role"
+                                        displayEmpty
+                                        inputProps={{ 'aria-label': 'Without label' }}
+                                        value={form.role}
+                                        onChange={handleChange}
+                                    >
+                                        <MenuItem value="">
+                                            <em></em>
+                                        </MenuItem>
+                                        <MenuItem value="Employee">Employee</MenuItem>
+                                        <MenuItem value="TeamLead">Team Lead</MenuItem>
+                                    </Select>
+                                </FormControl>
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign Up
+                        </Button>
+                        <Grid container justifyContent="center">
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    Already have an account? Sign in
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
+    );
 }
+
+export default SignUp;
